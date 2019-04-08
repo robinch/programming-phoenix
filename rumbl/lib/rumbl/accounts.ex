accd61743417b9aae88b48d3ff040d0cb6bcd037
Robin Chowdhury <robinch88@gmail.com>
2019-04-08 06:55:32 +0200

    Use ecto

---
+++ rumbl/lib/rumbl/accounts.ex
@@ -0,0 +1,34 @@
+defmodule Rumbl.Accounts do
+  @moduledoc """
+  The Accounts context.
+  """
+
+  alias Rumbl.Accounts.User
+  alias Rumbl.Repo
+
+  def get_user(id) do
+    Repo.get(User, id)
+  end
+
+  def get_user!(id) do
+    Repo.get!(User, id)
+  end
+
+  def get_user_by(params) do
+    Repo.get_by(User, params)
+  end
+
+  def list_users() do
+    Repo.all(User)
+  end
+
+  def create_user(attrs \\ %{}) do
+    %User{}
+    |> User.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def change_user(%User{} = user) do
+    User.changeset(user, %{})
+  end
+end
